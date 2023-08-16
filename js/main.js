0e736523b711ab201ecf10121cdb5a8106d4e78c
Szymon Dziewa <82220216+Szymofcion@users.noreply.github.com>
2023-08-16 21:24:56 +0200

    feat: :bug: fix animation in menu

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const nav = document.querySelector(".nav");
+const navItem2 = document.querySelector(".nav__item2");
 const navBtn = document.querySelector(".burger-btn");
 const allNavItems = document.querySelectorAll(".nav__item");
 const navBtnBars = document.querySelector(".burger-btn__bars");
@@ -6,12 +7,14 @@ const navBtnBars = document.querySelector(".burger-btn__bars");
 const handleNav = () => {
   nav.classList.toggle("nav--active");
   navBtnBars.classList.remove("black-bars-color");
+  navItem2.classList.toggle("nav-items-animation");
   allNavItems.forEach((item) => {
     item.classList.toggle("nav-items-animation");
   });
   allNavItems.forEach((item) => {
     item.addEventListener("click", () => {
       nav.classList.remove("nav--active");
+      navItem2.classList.remove("nav-items-animation");
       allNavItems.forEach((item) => {
         item.classList.remove("nav-items-animation");
       });
@@ -21,13 +24,11 @@ const handleNav = () => {
 
 const handleNavItemsAnimation = () => {
   let delayTime = 0;
-
   allNavItems.forEach((item) => {
     item.style.animationDelay = "." + (delayTime + 2) + "s";
     delayTime++;
   });
 };
-
 
 handleNavItemsAnimation();
 navBtn.addEventListener("click", handleNav);
